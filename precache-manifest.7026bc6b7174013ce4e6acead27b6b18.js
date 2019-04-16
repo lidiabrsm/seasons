7ffc50786a1b9336d25c16a834e624d02cfa151e
Lidia Brigido <lidiabrsm@gmail.com>
2019-04-16 00:52:45 -0300

    Updates

---
+++ precache-manifest.7026bc6b7174013ce4e6acead27b6b18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "808fed5ac1e88b432622",
+    "url": "/seasons/static/js/runtime~main.fbc5b39b.js"
+  },
+  {
+    "revision": "9d2568e4eedab0c45dfb",
+    "url": "/seasons/static/js/main.f080bd46.chunk.js"
+  },
+  {
+    "revision": "607320d00d64b68e18da",
+    "url": "/seasons/static/js/2.2050718e.chunk.js"
+  },
+  {
+    "revision": "9d2568e4eedab0c45dfb",
+    "url": "/seasons/static/css/main.46f28428.chunk.css"
+  },
+  {
+    "revision": "e276c2d1a3250f0e92eb536c9954728a",
+    "url": "/seasons/index.html"
+  }
+];
